bc4ff5f59126b62732e5fe093bb50c991faac136
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2021-06-23 21:34:46 -0400

    Update simple-fibonacci.py

--- simple-fibonacci.py
+++ simple-fibonacci.py
@@ -1,5 +1,15 @@
+number = input("Enter a number: ")
+print("Number to calculate is " + number)
+print(type(number))
+
+#conversion to float
+n = float(number)
+
+
+#fibonacci calculation
 def fibonacci(n):
     if n<2:
         return n
     else:
         return (fibonacci(n-1)+fibonacci(n-2))
+
